720141daeb366819a9070ad2587f2e2848b19314
thiagobastoscosta <thiagobastoscosta@gmail.com>
2021-06-28 12:50:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-v5etum
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-v5etum)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Olá mund!</h1>
+      <p>Feliz dia  :)</p>
+    </div>
+  );
+}
